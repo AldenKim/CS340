af79a0c344a2b405105b4515659fd5902e26e5c8
AldenKim <aldenkim.ut@gmail.com>
2025-01-07 13:41:09 -0700

    adding skeleton code for the functions required.

--- image-editor/ImageEditor.ts
+++ image-editor/ImageEditor.ts
@@ -1,3 +1,45 @@
 import { Color } from './Color';
 import { Image } from './Image';
 
+class ImageEditor {
+
+    public static main(args: String[]): void {
+
+    }
+
+    constructor() {
+        return;
+    }
+
+    public run(args: String[]): void {
+
+    }
+
+    private usage(): void {
+        console.log("USAGE: java ImageEditor <in-file> <out-file> <grayscale|invert|emboss|motionblur> {motion-blur-length}");
+    }
+
+    private motionblur(image: Image, length: number): void {
+
+    }
+
+    private invert(image: Image): void {
+
+    }
+
+    private grayscale(image: Image): void {
+
+    }
+
+    private emboss(image: Image): void {
+
+    } 
+
+    private read(filePath: String): Image {
+
+    }
+
+    private write(image: Image, filePath: String): void {
+        
+    }
+}
