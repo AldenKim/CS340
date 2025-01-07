90ec9fcaa820ab6d23c375d905f3bf55328f3167
AldenKim <aldenkim.ut@gmail.com>
2025-01-07 15:11:40 -0700

    adding the rest of the read functionality. May need to test.

--- image-editor/ImageEditor.ts
+++ image-editor/ImageEditor.ts
@@ -49,17 +49,35 @@ class ImageEditor {
     } 
 
     private read(filePath: string): Image {
-        let image = new Image(1,1);
+        let image = null;
 
         const fileContents = fs.readFileSync(filePath, 'utf-8');
-        const lineContents = fileContents.split(/\s|\n/).filter(line => line.trim() && !line.startsWith('#'));
+        const input = fileContents.split(/\s|\n/).filter(line => line.trim() && !line.startsWith('#'));
 
         // console.log(fileContents);
         // console.log(lineContents);
-        // console.log(lineContents[1]);
-        // console.log(lineContents[2]);
+        // console.log(input[1]);
+        // console.log(input[2]);
 
+        input.shift();
+
+        const width = Number(input.shift());
+        const height = Number(input.shift()); 
         
+        image = new Image(width, height);
+
+        input.shift();
+        
+        let i = 0;
+        for (let y = 0; y < height; ++y) {
+            for (let x = 0; x < width; ++x) {
+                const color = new Color();
+                color.red = Number(input[i++]);
+                color.green = Number(input[i++]);
+                color.blue = Number(input[i++]);
+                image.set(x,y,color);
+            }
+        }
 
         return image;
     }
