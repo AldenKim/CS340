6e4319433785da4eae7ae0c135a7ce3e28cca424
AldenKim <aldenkim.ut@gmail.com>
2025-02-19 14:40:42 -0700

    updates.

--- design-principles-starter-code/Employee.ts
+++ design-principles-starter-code/Employee.ts
@@ -1,6 +1,11 @@
 // 1. Explain how this program violates the High-Quality Abstraction principle.
-
+/* The Retirement calculator relies too much on the employee class information. 
+some of the functions should more likely be handled by the Employee class  */
 // 2. Explain how you would refactor the code to improve its design.
+/* I would move the functions getTotalYearsOfService and getMonthsInLastPosition into the
+employee class, the retirenment class should only be calculating retirenment and shouldn't worry about 
+these other functions. This would also allow retirementCalculator to not rely on one instance of employee when calculating
+retirement. */
 
 class Employee {
 	public employmentStartDate: Date;
