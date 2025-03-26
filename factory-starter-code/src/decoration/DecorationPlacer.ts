433da8fcad829d00948516a62882e74297fedb8c
AldenKim <aldenkim.ut@gmail.com>
2025-03-26 13:12:28 -0600

    Small change to exercise

--- factory-starter-code/src/decoration/DecorationPlacer.ts
+++ factory-starter-code/src/decoration/DecorationPlacer.ts
@@ -1,7 +1,4 @@
 import { DecorationFactory } from "../DecorationFactory";
-import { HalloweenTableclothPatternProvider } from "../holiday/Halloween/HalloweenTableclothPatternProvider";
-import { HalloweenWallHangingProvider } from "../holiday/Halloween/HalloweenWallHangingProvider";
-import { HalloweenYardOrnamentProvider } from "../holiday/Halloween/HalloweenYardOrnamentProvider";
 import { TableclothPatternProviderInterface } from "../holiday/TableclothPatternProviderInterface";
 import { WallHangingProviderInterface } from "../holiday/WallHangingProviderInterface";
 import { YardOrnamentProviderInterface } from "../holiday/YardOrnamentProviderInterface";
